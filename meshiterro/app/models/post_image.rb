ad08327c513de6d0f61217dae0f3bb3e3e4850d4
EC2 Default User <ec2-user@ip-172-31-0-202.ap-northeast-1.compute.internal>
2022-06-09 08:18:09 +0000

    4-18

--- meshiterro/app/models/post_image.rb
+++ meshiterro/app/models/post_image.rb
@@ -2,13 +2,14 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  # def get_image
-  #   if image.attached?
-  #     image
-  #   else
-  #     'no_image.jpg'
-  #   end
-  # end
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
 
   def get_image
     unless image.attached?
@@ -17,6 +18,5 @@ class PostImage < ApplicationRecord
     end
     image
   end
-
 
 end
